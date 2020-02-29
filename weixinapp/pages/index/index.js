a694443bc532c8616e84216e094a75b696932ca8
neozhu <new163@163.com>
2020-02-29 19:09:57 +0800

    update

--- weixinapp/pages/index/index.js
+++ weixinapp/pages/index/index.js
@@ -2,21 +2,21 @@
 //获取应用实例
 const app = getApp()
 import drawQrcode from 'weapp-qrcode'
-import QR from '../../common/weapp-qrcode'  
+import QR from '../../common/weapp-qrcode'
 import Page from '../../common/page';
 Page({
   data: {
     userInfo: {},
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     hasUserInfo: false,
-    showfriends:false,
-    showvips:false,
+    showfriends: false,
+    showvips: false,
     qrcodeURL: '',
-    foreground:'#000',
+    foreground: '#000',
     width: '100vw',
     height: '180',
     text: '本人就餐',
-    dot:true,
+    dot: true,
     friends: '',
     vips: '',
     code: 'http:www.google.com',
@@ -52,15 +52,15 @@ Page({
         }
       })
     }
-    
+
   },
-  onClose(){
+  onClose() {
     this.setData({
-      showfriends:false,
-      showvips:false
+      showfriends: false,
+      showvips: false
     })
   },
-  onSetFriends(e){
+  onSetFriends(e) {
     console.log(e)
     this.setData({
       showfriends: false,
@@ -69,13 +69,13 @@ Page({
     })
     this.draw()
   },
-  onSetVips(e){
+  onSetVips(e) {
     console.log(e)
     this.setData({
-      vips:e.detail
+      vips: e.detail
     })
   },
-  onConfirmVips(){
+  onConfirmVips() {
     this.setData({
       showvips: false,
       friends: '',
@@ -85,10 +85,10 @@ Page({
   selfDining() {
     this.setData({
       foreground: '#000',
-      text:'本人就餐',
-      dot:true,
-      friends:'',
-      vips:'',
+      text: '本人就餐',
+      dot: true,
+      friends: '',
+      vips: '',
       code: new Date().toString()
     })
     this.draw()
@@ -97,59 +97,72 @@ Page({
     this.setData({
       foreground: '#1CA4FC',
       dot: false,
-      showfriends:true,
+      showfriends: true,
       text: '家属就餐',
       code: new Date().toString()
     })
-    
+
   },
   vipDining() {
     this.setData({
       foreground: '#FF6F00',
       dot: false,
-      showvips:true,
+      showvips: true,
       friends: '',
       vips: '1',
       text: '陪同人员就餐',
       code: new Date().toString()
     })
-     
+
   },
   draw: function () {
-    const imgData = QR.drawImg(this.data.code, {
-      typeNumber: 4,
-      errorCorrectLevel: 'M',
-      size: 500
-    })
-    //console.log(imgData)
-    this.setData({qrcodeURL : imgData})
+    // const imgData = QR.drawImg(this.data.code, {
+    //   typeNumber: 4,
+    //   errorCorrectLevel: 'M',
+    //   size: 500
+    // })
+    // //console.log(imgData)
+    // this.setData({ qrcodeURL: imgData })
     // console.log(this.data.userInfo.avatarUrl)
     // const that = this
-    // drawQrcode({
-    // width: 220,
-    // height: 220,
-    //   foreground: this.data.foreground,
-    // canvasId: 'myQrcode',
-    // text: this.data.code,
-    // image: {
-    //   imageResource: this.data.userInfo.avatarUrl,
-    //     dx: 50,
-    //     dy: 50,
-    //     dWidth: 40,
-    //     dHeight: 40
-    // },
-    // callback: function(e) {
-    //   console.log(e)
-    //   //that.$apply()
-    // }
-    // })
+    drawQrcode({
+      width: 220,
+      height: 220,
+      foreground: this.data.foreground,
+      typeNumber:-1,
+      canvasId: 'myQrcode',
+      text: this.data.code,
+      image: {
+        imageResource: this.data.userInfo.avatarUrl,
+        dx: 50,
+        dy: 50,
+        dWidth: 40,
+        dHeight: 40
+      },
+      callback: e => {
+        //console.log(e)
+        wx.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width: 220,
+          height: 220,
+          canvasId: 'myQrcode',
+          success:  res => {
+            console.log(res.tempFilePath);
+            this.setData({
+              qrcodeURL: res.tempFilePath
+            })
+          }
+        })
+        }
+    })
   },
-getUserInfo: function(e) {
-  console.log(e)
-  app.globalData.userInfo = e.detail.userInfo
-  this.setData({
-    userInfo: e.detail.userInfo,
-    hasUserInfo: true
-  })
-}
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
 })
